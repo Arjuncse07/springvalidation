e1d0518212a97da7dd106594bf8b84031ba8cfb9
Arjun Singh <aj629280@gmail.com>
2023-04-15 10:05:57 +0530

    Arjun Commits code

---
+++ src/main/java/com/techtree/springvalidation/Controller/MyController.java
@@ -0,0 +1,43 @@
+package com.techtree.springvalidation.Controller;
+
+import com.techtree.springvalidation.Model.LoginData;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class MyController {
+
+   @GetMapping("/form")
+    public String openForm(Model model){
+
+       model.addAttribute("loginData",new LoginData());
+
+        return "form";
+    }
+
+
+    //handler for processing form
+    @PostMapping("/process")
+    public String processForm(@Valid @ModelAttribute("loginData") LoginData loginData, BindingResult result)  {
+
+       if(result.hasErrors()){
+           System.out.println(result);
+           return "form";
+       }
+
+        System.out.println(loginData);
+
+       //data process
+
+       return "success";
+    }
+
+
+
+}
